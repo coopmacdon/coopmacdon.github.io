8729a334d865924a2ae3015903673c4d49420323
coopmacdon <cooper.macdonald@dcmail.ca>
2023-11-24 16:28:38 -0500

    lab 4 part 4
    finished but the evil eye is not working correctly.

--- lab4/part4/main.js
+++ lab4/part4/main.js
@@ -5,8 +5,11 @@ Date: November 24, 2023
 Description: Java Script that displays colored balls on a black background, as the balls move they can collide with each other and change color.
 */
 
-// Constants 
+// Array for balls
 const balls = []
+
+const para = document.querySelector('canvas')
+let count = 0;
 
 // setup canvas
 
@@ -90,6 +93,76 @@ class Ball extends Shape{
         }
     }
 }
+
+class EvilCircle extends Shape {
+
+    constructor(x, y) {
+        super(x, y, 20, 20);
+    
+        this.color = "white";
+        this.size = 10;
+    
+        window.addEventListener('keydown', (e) => {
+            switch(e.key) {
+            case 'a':
+                this.x -= this.velX;
+                break;
+            case 'd':
+                this.x += this.velX;
+                break;
+            case 'w':
+                this.y -= this.velY;
+                break;
+            case 's':
+                this.y += this.velY;
+                break;
+            }
+        });
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.strokeStyle = this.color;
+        ctx.lineWidth = 3;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.stroke();
+    }
+
+    checkBounds() {
+        if ((this.x + this.size) >= width){
+            this.x -= this.size;
+        }
+        if ((this.x - this.size) <= 0){
+            this.x += this.size;
+        }
+        if ((this.y + this.size) >= height){
+            this.y -= this.size;
+        }
+        if ((this.y - this.size) <= 0){
+            this.y += this.size;
+        }
+    }
+
+    collisionDetect() {
+        for (const ball of balls) {
+          if (ball.exists) {
+                const dx = this.x - ball.x;
+                const dy = this.y - ball.y;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+        
+                if (distance < this.size + ball.size) {
+                ball.exists = false;
+                count--;
+                para.textContent = 'Ball count: ' + count;
+                }
+            }
+        }
+    }
+    
+}
+// Evil eye circle
+const evilBall = new EvilCircle(random(0, width), random(0, height));
+
 while (balls.length < 25) {
     const size = random(10,20);
     const ball = new Ball(
@@ -102,6 +175,8 @@ while (balls.length < 25) {
     );
 
     balls.push(ball);
+    count++;
+    para.textContent = 'Ball count: ' + count;
 }
 
 function loop() {
@@ -113,6 +188,11 @@ function loop() {
         ball.update();
         ball.collisionDetect();
     }
+
+    evilBall.draw();
+    evilBall.checkBounds();
+    evilBall.collisionDetect();
+    
 
     requestAnimationFrame(loop);
 }
