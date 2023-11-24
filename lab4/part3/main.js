fa518c78df81fe5363c00b6c5970e6b5c4dfdb42
coopmacdon <cooper.macdonald@dcmail.ca>
2023-11-24 15:35:12 -0500

    lab 4 part 3
    finished the implementation of the loop, collision detection and update to accurately display the balls moving and changing colour.

--- lab4/part3/main.js
+++ lab4/part3/main.js
@@ -2,8 +2,11 @@
 Author: Cooper MacDonald 
 File: main.js
 Date: November 24, 2023
-Description: 
+Description: Java Script that displays colored balls on a black background, as the balls move they can collide with each other and change color.
 */
+
+// Constants 
+const balls = []
 
 // setup canvas
 
@@ -35,11 +38,74 @@ class Ball {
         this.color =color;
         this.size = size;
     }
+
+
+    drawBall() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+
+    update() {
+        if ((this.x + this.size) >= width) {
+        this.velX = -(Math.abs(this.velX));
+        }
+
+        if ((this.x - this.size) <= 0) {
+        this.velX = Math.abs(this.velX);
+        }
+
+        if ((this.y + this.size) >= height) {
+        this.velY = -(Math.abs(this.velY));
+        }
+
+        if ((this.y - this.size) <= 0) {
+        this.velY = Math.abs(this.velY);
+        }
+
+        this.x += this.velX;
+        this.y += this.velY;
+    }
+
+    collisionDetect() {
+        for (const ball of balls) {
+            if (!(this === ball)) {
+                const dx = this.x - ball.x;
+                const dy = this.y - ball.y;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+                if (distance < this.size + ball.size){
+                    ball.color = this.color = randomRGB();
+                }
+            }
+        }
+    }
+}
+while (balls.length < 25) {
+    const size = random(10,20);
+    const ball = new Ball(
+        random(0 + size,width - size),
+        random(0 + size,height - size),
+        random(-7,7),
+        random(-7,7),
+        randomRGB(),
+        size
+    );
+
+    balls.push(ball);
 }
 
-drawBall(); {
-    ctx.beginPath();
-    ctx.fillStyle = this.color;
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    ctx.fill();
+function loop() {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+    ctx.fillRect(0, 0, width, height);
+
+    for (const ball of balls) {
+        ball.drawBall();
+        ball.update();
+        ball.collisionDetect();
+    }
+
+    requestAnimationFrame(loop);
 }
+
+loop();
